e67cad1b514dd6a5b9cd94f9c5d246a472b59e8c
0x8anon <avdbergbusiness@skiff.com>
2023-11-11 23:27:48 +0200

    changes

--- _printf.c
+++ _printf.c
@@ -1,27 +1,31 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _printf - a function that produces output according to format
  * @format: a character string that handles conversion specifiers
  * Return: numbers of characters printed
 */
-
 int _printf(const char *format, ...)
 {
-	int count;
-	va_list args;
+    int count = 0;
+    va_list ap;
 
-	va_start(args, format);
-	while (*format != '\0')
-	{
-		if (*format == '%')
-			count += print_format(*(++format), args);
-		else
-		{
-			count += write(1, format, 1);
-			++format;
-		}
-	}
-	va_end(args);
+    va_start(ap, format);
+
+    while (format && *format != '\0')
+    {
+        if (*format == '%')
+        {
+            count += print_format(*(++format), ap);
+        }
+        else
+        {
+            count += print_char(*format);
+            ++format;
+        }
+    }
+
+    va_end(ap);
+
+    return count;
 }
