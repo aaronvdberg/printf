82d3370cd76bbdf571770d399edd87432d4008c5
0x8anon <avdbergbusiness@skiff.com>
2023-11-11 14:44:46 +0200

    moved stuff to tests folder

--- _printf.c
+++ _printf.c
@@ -9,10 +9,22 @@
 
 int _printf(const char *format, ...)
 {
+int count; 
 
+va_list ap; 
 
+va_start(ap, format);
+count = 0;
 
+while(*format != '\0')
+{
+	if (*format == '%')
+	{	
+	count += _printf(*(++format), ap);
+	
+	}
 
+}
 
 
 }
