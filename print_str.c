bd3212da3992a1514f4a1971a212d27ec6ea81b1
askmoud <116741927+askmoud@users.noreply.github.com>
2023-11-12 15:40:40 +0100

    Update print_str.c

--- print_str.c
+++ print_str.c
@@ -1,32 +1,13 @@
 #include "main.h"
 /**
  * print_str - Print a string to standard output
-<<<<<<< HEAD
- * @ap 
- *
+ * @ap
 */
 
 int print_str (va_list ap)
 {
-	int i;
-	char *str;
-
-	str = va_arg(ap, char *);
-	if (str == NULL)
-	{
-		str = ("Nothing entered");
-		return (_puts(str));
-	}
-=======
- * @ap
- */
-
-
-int print_str (va_list ap)
-{
 int i;
-char *str;1
->>>>>>> fad2b2b (betty fix)
+char *str;
 
 	for (i = 0; str[i]; i++)
 	{
