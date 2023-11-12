76fce58961f2c98aca58a955748c593c9cbd25d1
askmoud <116741927+askmoud@users.noreply.github.com>
2023-11-12 15:16:10 +0100

    Update _printf.c
    Fixing betty style error

--- _printf.c
+++ _printf.c
@@ -7,11 +7,11 @@
 */
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    va_list ap;
-    va_start(ap, format);
+	int count = 0;
+	va_list ap;
+	va_start(ap, format);
 
-    
+	
 if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 {
 return (-1);
@@ -19,31 +19,31 @@ return (-1);
 
 while (format && *format)
 {
-        if (*format == '%') {
-            format++;
-            switch (*format) {
-                case 'c':
-                    count += print_char(ap);
+		if (*format == '%') {
+			format++;
+			switch (*format) {
+				case 'c':
+					count += print_char(ap);
 		break;
-                case 's':
-                    count += print_str(ap);
-                    break;
-                case '%':
-                    _putchar('%');
-                    count++;
-                    break;
-                default:
-                    _putchar('%');
-                    _putchar(*format);
-                   count += 2;
-            }
-            format++;
-        } else {
-            _putchar(*format);
-            count++;
-            format++;
-        }
-    }
-    va_end(ap);
-    return (count);
+				case 's':
+					count += print_str(ap);
+					break;
+				case '%':
+					_putchar('%');
+					count++;
+					break;
+				default:
+					_putchar('%');
+					_putchar(*format);
+				   count += 2;
+			}
+			format++;
+		} else {
+			_putchar(*format);
+			count++;
+			format++;
+		}
+	}
+	va_end(ap);
+	return (count);
 }
