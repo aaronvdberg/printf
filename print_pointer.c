a48ed932a71d4b54b3eaae2958d3391cf689d2b0
askmoud <hamzaaskmoud27@gmail.com>
2023-11-14 14:52:41 +0100

    new changes, the pointer function not working yet

--- print_pointer.c
+++ print_pointer.c
@@ -1,18 +1,20 @@
 #include "main.h"
+
 /**
- * print_pointer - print a pointer address in hexadecimal format
- * @ap: argument list
- * Return: number of characters printed
+ * print_pointer - A function to print pointer address.
+ * @ap: va_list that contains the pointer argument.
+ * Return: Number of characters printed.
  */
 int print_pointer(va_list ap)
 {
 	void *ptr = va_arg(ap, void*);
+	unsigned long int address = (unsigned long int)ptr;
 	int count = 0;
 
-	_putchar('0');
-	_putchar('x');
-	count += 2;
-	count += print_positive_hex((unsigned long)ptr, 1);
+	count += _putchar('0');
+	count += _putchar('x');
+
+	count += _printf("%016lx", address);
 
 	return (count);
 }
