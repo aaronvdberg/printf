8efeb7930b7fab11e1406b1ddda416166357f91b
0x8anon <avdbergbusiness@skiff.com>
2023-11-14 16:36:36 +0200

    readded _puts.c and _putchar.c

---
+++ _puts.c
@@ -0,0 +1,20 @@
+/**
+ * _puts - Prints a string to the standard output.
+ * @str: The string to be printed.
+ *
+ * Return: The number of characters printed.
+ */
+
+int _puts(const char *str)
+{
+    int count = 0;
+
+    while (*str)
+    {
+        write(1, str, 1);
+        str++;
+        count++;
+    }
+
+    return count;
+}
