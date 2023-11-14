a67686640034b76a5ee2aa7d41a96f2b0fd0c6cb
0x8anon <avdbergbusiness@skiff.com>
2023-11-14 20:01:20 +0200

    fixed my fuckup, added flags

---
+++ get_flags.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+int get_flags(const char *format, int *i)
+{
+    const char FLAGS_CH[] = {'+', ' ', '#', '\0'};
+    const int FLAGS_ARR[] = {F_PLUS, F_SPACE, F_HASH};
+
+    int flags = 0;
+    int curr_i = *i + 1;
+
+    while (format[curr_i] != '\0')
+    {
+        char current_flag = format[curr_i];
+
+        switch (current_flag) 
+        {
+            case '+':
+            case '#':
+            case ' ':
+                for (int j = 0; FLAGS_CH[j] != '\0'; j++) 
+                {
+                    if (current_flag == FLAGS_CH[j]) {
+                        flags |= FLAGS_ARR[j];
+                        break;
+                    }
+                }
+                break;
+            default:
+                curr_i--;
+                break;
+        }
+
+        curr_i++;
+    }
+
+    *i = curr_i - 1;
+
+    return flags;
+}
