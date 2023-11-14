30d656ea03047d4578689e30e9cb2c0b686d4b17
0x8anon <avdbergbusiness@skiff.com>
2023-11-14 16:43:15 +0200

    merged

--- print_pointer.c
+++ print_pointer.c
@@ -1,9 +1,5 @@
 #include "main.h"
-<<<<<<< HEAD
 #include <stdint.h>
-=======
-
->>>>>>> askmoud-main
 /**
  * print_pointer - a function to print a pointer address
  * @ap: the va_list that contains the pointer argument
@@ -12,13 +8,6 @@
 
 int print_pointer(va_list ap)
 {
-<<<<<<< HEAD
-	int count = 0;
-	unsigned long ptr_ad = (unsigned long)va_arg(ap, void *);
-	count += _putchar('0');
-	count += _putchar('x');
-	count += print_positive_hex(ptr_ad, sizeof(void *) * 2);
-=======
 	void *ptr = va_arg(ap, void *);
 	char *hex_prefix = "0x";
 	int count = 0;
@@ -33,6 +22,5 @@ int print_pointer(va_list ap)
 		count += print_hex_lower_helper((unsigned long)ptr, 1);
 	}
 
->>>>>>> askmoud-main
 	return (count);
 }
